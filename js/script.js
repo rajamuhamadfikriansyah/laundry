c0120ed06ee39579911ddb30bd218ae52d0251b2
rajamuhamadfikriansyah <rajamuhamadfikriansyah@github.com>
2024-05-15 20:50:52 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-//TOGGLE
-const navbarNav = document.querySelector
-('.navbar-nav');
-//ketika menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
-}
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika di Menu sidebar di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+const hamburger =  document.querySelector ('#hamburger-menu');
